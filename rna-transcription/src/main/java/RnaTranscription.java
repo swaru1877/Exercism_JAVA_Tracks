40c8b44d55d4af69e24e07581f884c49d339be7b
swaroopa a <anisetti.swaroopa@thoughtworks.com>
2019-07-12 12:01:57 +0530

    [AS]RnaTranscription

---
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+class RnaTranscription {
+
+    String transcribe(String dnaStrand) {
+
+    	//Maping of DNA to RNA using HashMap
+    	HashMap<Character, String> dnaMap = new HashMap<Character, String>();
+		dnaMap.put('A', "U");
+        dnaMap.put('G', "C");
+        dnaMap.put('C', "G");
+        dnaMap.put('T', "A");
+
+		StringBuilder result = new StringBuilder();
+        char[] dnaStrandCharArray = dnaStrand.toCharArray();	//Converting String to char array
+
+        for(char dnaS : dnaStrandCharArray){
+
+        	result.append(dnaMap.get(dnaS));
+        }
+        return(result.toString());	//Returning rnaStrand
+    }
+
+}
