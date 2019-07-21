c367f7cab6ced3fce6d53c127bd9136d604683db
swaroopa a <anisetti.swaroopa@thoughtworks.com>
2019-07-21 11:59:15 +0530

    updated palindrome

--- is-palindrome-master/src/main/java/Palindrome.java
+++ is-palindrome-master/src/main/java/Palindrome.java
@@ -1,9 +1,9 @@
 public class Palindrome {
     public boolean check(String sentence) {
 
-		Palindrome obj = new Palindrome();
-    	sentence = sentence.replaceAll("[^a-zA-Z]", "");
-        return((sentence.equalsIgnoreCase(obj.reverseOfString(sentence)))) ;
+    Palindrome obj = new Palindrome();
+    sentence = obj.lowerCase(sentence.replaceAll("[^a-zA-Z]", ""));
+    return(compare(sentence,obj.reverseOfString(sentence))) ;
 
 	}
 
@@ -11,7 +11,25 @@ public class Palindrome {
         String strReverse = "";
         for(char ch : inputString.toCharArray())
             strReverse = ch + strReverse;
-        
         return strReverse;
     }
+
+    boolean compare(String str, String strRev){
+        for(int i = 0; i < str.length(); i++){
+            if (str.charAt(i) != strRev.charAt(i))
+                return false;
+        }
+        return true;
+    }
+
+    String lowerCase(String str){
+        String strLowerCase = "";
+        for(char ch: str.toCharArray()) {
+             if(ch >= 'A' && ch <= 'Z')
+                 ch += 'a' - 'A';
+             strLowerCase += ch;
+        }
+        return strLowerCase;
+    }
+ 
 }
